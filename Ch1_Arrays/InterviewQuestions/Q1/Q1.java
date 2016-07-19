37679695e5d6041e471667b99d6e2b7ac7eea99d
Tim Dickson <gteldirith543@gmail.com>
2016-07-19 13:27:39 -0500

    first commit

---
+++ Ch1_Arrays/InterviewQuestions/Q1/Q1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Q1{
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        String theString = reader.next();
+
+        boolean value = isUnique(theString);
+        printOutput(theString, value);
+    }
+    public static boolean isUnique(String aString){
+        
+    }
+    public static void printOutput(boolean aValue){
+
+    }
+}
+
+
+
